23fa2fa080703df01d2e8bec551bfa10ca39c7f4
thypedy <84384293+thypedy@users.noreply.github.com>
2023-05-03 12:20:44 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+import Login from "./pages/Login";
+
+
+function App() {
+  return (
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/sobre" element={<Sobre/>}/>
+          <Route path="/contato" element={<Contato/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+        </BrowserRouter>
+
+      </div>
+  )
+}
+
+export default App
